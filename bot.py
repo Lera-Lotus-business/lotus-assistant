d24541cc442d8e30df341ded62aef7f49ca4e5ed
VALERIIA <Silaluni@gmail.com>
2025-07-01 15:41:19 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,16 @@
 from aiogram import Bot, Dispatcher, executor, types
-import logging
+from handlers import start, main  # <-- добавили main
 from config import BOT_TOKEN
-
-from handlers import start, digest, info
+import logging
 
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
+# Регистрируем обработчики команд
 start.register_handlers(dp)
-digest.register_handlers(dp)
-info.register_handlers(dp)
+main.register_handlers(dp)  # <-- добавили регистрацию нового файла
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
